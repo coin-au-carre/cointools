3c8b1e4aa4bdc6167026943401c9e22ecc98bb16
Florian D <florian.coin@gmail.com>
2015-07-15 23:12:09 +0200

    forgot adding type list hpp...

--- include/coin/algorithm.hpp
+++ include/coin/algorithm.hpp
@@ -104,7 +104,7 @@ template<typename T>
 constexpr T max(T a, T b ) { return a < b ? b : a; }
 
 template <typename T>
-inline T    max(const T& t) { return t; }
+inline    T max(const T& t) { return t; }
 
 template <typename T, typename ...P>
 inline auto max(const T& t, const P& ...p) {
