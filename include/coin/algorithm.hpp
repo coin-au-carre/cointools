c91167f9b648319c32b8ad2e27226c0e1e873692
Florian D <florian.coin@gmail.com>
2015-08-01 15:07:51 +0200

    adding multi-grid v1

--- include/coin/algorithm.hpp
+++ include/coin/algorithm.hpp
@@ -88,36 +88,6 @@ bool multi_dim_counter(std::vector<T>& v, const std::vector<T>& lower, const std
 }
 
 
-template<typename T> 
-constexpr T min(T a, T b ) { return a > b ? b : a; }
-
-template <typename T>
-inline T    min(const T& t) { return t; }
-
-template <typename T, typename ...P>
-inline auto min(const T& t, const P& ...p) {
-	using res_type= std::common_type_t<T, P...>;
-	return std::min(res_type(t), res_type(max(p...)));
-}
-
-template<typename T> 
-constexpr T max(T a, T b ) { return a < b ? b : a; }
-
-template <typename T>
-inline    T max(const T& t) { return t; }
-
-template <typename T, typename ...P>
-inline auto max(const T& t, const P& ...p) {
-	using res_type= std::common_type_t<T, P...>;
-	return std::max(res_type(t), res_type(max(p...)));
-}
-
-template<typename T>
-constexpr T sum(T x) { return x; }
-
-template<typename T, typename... Ts>
-T sum(T x, Ts... xs) { return x + sum(xs...); }
-
 } // namespace _impl_algorithm
 
 using _impl_algorithm::begin;
@@ -128,9 +98,6 @@ using _impl_algorithm::remove_element;
 using _impl_algorithm::lower_bound_index;
 using _impl_algorithm::upper_bound_index;
 using _impl_algorithm::multi_dim_counter;
-using _impl_algorithm::min;
-using _impl_algorithm::max;
-using _impl_algorithm::sum;
 
 
 } // namespace coin
--- include/coin/math.hpp
+++ include/coin/math.hpp
@@ -6,6 +6,46 @@ namespace coin {
 
 namespace _impl_math {
 
+template<typename T> 
+constexpr T min(T a, T b ) { return a > b ? b : a; }
+
+template <typename T>
+inline T    min(const T& t) { return t; }
+
+template <typename T, typename ...P>
+inline auto min(const T& t, const P& ...p) {
+	using res_type= std::common_type_t<T, P...>;
+	return std::min(res_type(t), res_type(max(p...)));
+}
+
+template<typename T> 
+constexpr T max(T a, T b ) { return a < b ? b : a; }
+
+template <typename T>
+inline    T max(const T& t) { return t; }
+
+template <typename T, typename ...P>
+inline auto max(const T& t, const P& ...p) {
+	using res_type= std::common_type_t<T, P...>;
+	return std::max(res_type(t), res_type(max(p...)));
+}
+
+template<typename T>
+constexpr T sum(T x) { return x; }
+
+template<typename T, typename... Ts>
+constexpr T sum(T x, Ts... xs) { return x + sum(xs...); }
+
+
+template<typename T>
+constexpr T meta_prod(T x) { return x; }
+ 
+template<typename T, typename... Ts>
+constexpr T meta_prod(T x, Ts... xs) { return x * meta_prod(xs...); }
+
+template<typename T, typename E>
+constexpr T meta_pow(T base, E expo) { return (expo != 0 ) ? base * meta_pow(base, expo-1) : 1; }
+
 template<class T>
 constexpr T abs(T x){
 	return x < 0 ? -x : x;
@@ -34,6 +74,11 @@ void set_max_decimal_digits() {
 
 } // namespace _impl_math
 
+using _impl_math::min;
+using _impl_math::max;
+using _impl_math::sum;
+using _impl_math::meta_prod;
+using _impl_math::meta_pow;
 using _impl_math::abs;
 using _impl_math::sqrt;
 using _impl_math::deg_to_rad;
